cc1badeefb71ddf519de31bbaa42f3af5e03e8b7
DeanHasani <hasanidean97@gmail.com>
2025-09-21 21:51:58 +0200

    added photo in public folder

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,8 @@
+import Image from 'next/image';
+
+
 export default function Page() {
-  return <h1>Hello, Next.js!</h1>
+  return <div>
+  <Image src="/nextjs.svg" alt="NextLogo" width={100} height={100} /> <h1>Hello, Next.js!</h1>
+  </div>
 }
