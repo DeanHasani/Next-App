de75c295a6a9f19a1bcfe4002e6ed76296b49eda
DeanHasani <hasanidean97@gmail.com>
2025-09-22 18:20:13 +0200

    project structure tryouts

---
+++ src/app/(admin)/dashboard/page.js
@@ -0,0 +1,5 @@
+export default function Page() {
+  return <div>
+  <h1>Admin Dashboard</h1>
+  </div>
+}
---
+++ src/app/(marketing)/about/page.js
@@ -0,0 +1,5 @@
+export default function Page() {
+  return <div>
+  <h1>About Dashboard</h1>
+  </div>
+}
